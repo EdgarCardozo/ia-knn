7be0359b6ba04093b29d34f0b5850a7b6533aee8
edgar.cardozo <edgar.cardozo@globant.com>
2020-10-20 12:59:55 -0300

    Fixing limits

--- src/main/java/com/ia/knn/infrastructure/service/DefaultKnnService.java
+++ src/main/java/com/ia/knn/infrastructure/service/DefaultKnnService.java
@@ -142,7 +142,7 @@ public class DefaultKnnService implements KnnService {
     double xMax = elements.stream()
             .max(Comparator.comparing(Element::getX)).orElseThrow(RuntimeException::new)
             .getX();
-    double yMax = elements.stream()g
+    double yMax = elements.stream()
             .max(Comparator.comparing(Element::getY)).orElseThrow(RuntimeException::new)
             .getY();
 
